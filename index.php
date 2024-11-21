132dbb66c1557016358df07263dc1b7a0ef980a4
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2024-11-21 20:22:45 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-slate-900 text-white">
+    <header class="mx-auto max-w-screen-lg px-3 py-6 flex justify-between">
+        <div class="font-bold text-xl text-cyan-600">
+            🐧 Meu Portfolio ...
+        </div>
+        <div>
+            <ul class="flex gap-x-3 font-medium text-gray-200">
+                <li><a href="" class="hover:underline">Projetos</a></li>
+                <li><a href="" class="hover:underline">Github</a></li>
+                <li><a href="" class="hover:underline">Linkedin</a></li>
+            </ul>
+        </div>
+    </header>
+    <main class="mx-auto max-w-screen-lg min-h-20 px-3 py-3">
+        <!--  Hero-->
+        <section class="flex gap-x-3">
+            <div class="w-2/3">
+                <h1 class="text-3xl font-bold">Oi, meu nome é Kerlon!</h1>
+                <p class="text-xl leading-6 mt-6">
+                    Falando um pouco sobre mim, sou um desenvolvedor web que adora criar soluções novas para problemas do mundo real focando na esperiencia do usuário.
+                    Atualmente, trabalho como Técnico de apoio ao usuário de informática (Service Desk), mas também possuo conhecimento na area de desenvolvimento, como PHP, HTML, CSS, JavaScript, e na área de Bando de Dados, SQL, XAMPP.
+                </p>
+                <ul class="flex gap-x-3 mt-3">
+                    <li><a href=""><img src="./assets/facebook.png" alt="Facebook"></a></li>
+                    <li><a href=""><img src="./assets/whatsapp.png" alt="Whatsapp"></a></li>
+                    <li><a href=""><img src="./assets/github.png" alt="Github"></a></li>
+                    <li><a href=""><img src="./assets/linkedin.png" alt="Linkedin"></a></li>
+                </ul>
+            </div>
+            <div class="w-1/3 flex items-center justify-center">
+                <div>
+                    foto
+                </div>
+            </div>
+        </section>
+        <!-- Projetos -->
+         <section class="space-y-3 py-6">
+            <h2 class="text-2xl font-bold">Meus projetos</h2>
+            <!-- projeto -->
+            <div class="bg-slate-800 rounded-lg p-3 flex items-center">
+                <div class="w-1/5">foto</div>
+                <div class="w-4/5 space-y-3">
+                    <div class="flex gap-3 justify-between">
+                        <h3 class="font-semibold text-xl">✅ Projeto 1 <span class="text-xs text-gray-400 opacity-50 italic"> (finalizado em 2024)</span></h3>
+                        <div>
+                            <span class="bg-fuchsia-400 rounded-md px-2 py-1 font-semibold text-xs text-fuchsia-900">PHP</span>
+                            <span class="bg-lime-400 rounded-md px-2 py-1 font-semibold text-xs text-lime-900">JavaScript</span>
+                            <span class="bg-sky-400 rounded-md px-2 py-1 font-semibold text-xs text-sky-900">HTML</span>
+                            <span class="bg-rose-400 rounded-md px-2 py-1 font-semibold text-xs text-rose-900">CSS</span>
+                        </div>
+                    </div>
+                    <p class="leading-6">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam necessitatibus unde facere consequuntur voluptas quasi! Ex, veritatis quis? Aliquid officia accusantium reiciendis laudantium nesciunt deleniti voluptatibus sequi illo dolorum et.
+                        </p>
+                </div>
+            </div>
+            <div class="bg-slate-800 rounded-lg p-3 flex items-center">
+                <div class="w-1/5">foto</div>
+                <div class="w-4/5 space-y-3">
+                    <div class="flex gap-3 justify-between">
+                        <h3 class="font-semibold text-xl">✅ Projeto 2 <span class="text-xs text-gray-400 opacity-50 italic"> (finalizado em 2024)</span></h3>
+                        <div>
+                            <span class="bg-fuchsia-400 rounded-md px-2 py-1 font-semibold text-xs text-fuchsia-900">PHP</span>
+                            <span class="bg-lime-400 rounded-md px-2 py-1 font-semibold text-xs text-lime-900">JavaScript</span>
+                            <span class="bg-sky-400 rounded-md px-2 py-1 font-semibold text-xs text-sky-900">HTML</span>
+                            <span class="bg-rose-400 rounded-md px-2 py-1 font-semibold text-xs text-rose-900">CSS</span>
+                        </div>
+                    </div>
+                    <p class="leading-6">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam necessitatibus unde facere consequuntur voluptas quasi! Ex, veritatis quis? Aliquid officia accusantium reiciendis laudantium nesciunt deleniti voluptatibus sequi illo dolorum et.
+                        </p>
+                </div>
+            </div>
+            <div class="bg-slate-800 rounded-lg p-3 flex items-center">
+                <div class="w-1/5">foto</div>
+                <div class="w-4/5 space-y-3">
+                    <div class="flex gap-3 justify-between">
+                        <h3 class="font-semibold text-xl">✅ Projeto 3 <span class="text-xs text-gray-400 opacity-50 italic"> (finalizado em 2024)</span></h3>
+                        <div>
+                            <span class="bg-fuchsia-400 rounded-md px-2 py-1 font-semibold text-xs text-fuchsia-900">PHP</span>
+                            <span class="bg-lime-400 rounded-md px-2 py-1 font-semibold text-xs text-lime-900">JavaScript</span>
+                            <span class="bg-sky-400 rounded-md px-2 py-1 font-semibold text-xs text-sky-900">HTML</span>
+                            <span class="bg-rose-400 rounded-md px-2 py-1 font-semibold text-xs text-rose-900">CSS</span>
+                        </div>
+                    </div>
+                    <p class="leading-6">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam necessitatibus unde facere consequuntur voluptas quasi! Ex, veritatis quis? Aliquid officia accusantium reiciendis laudantium nesciunt deleniti voluptatibus sequi illo dolorum et.
+                        </p>
+                </div>
+            </div>
+         </section>
+    </main>
+    <footer class="mx-auto max-w-screen-lg min-h-20">
+        <div class="border-t border-gray-600 pt-6 px-3 text-gray-400 text-sm">
+            @ Copyright 2024. Kerlon Hinterholz.
+        </div>
+    </footer>
+</body>
+</html> 
