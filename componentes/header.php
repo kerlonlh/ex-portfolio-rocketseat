47f904718e8ecfda5671a3fe61b480d840c290f0
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2024-11-22 16:43:32 -0300

    Deixando o header dinâmico

--- componentes/header.php
+++ componentes/header.php
@@ -1,12 +1,25 @@
+<?php 
+    $itens = [
+        ['href' => '#projetos', 'target'=> '', 'texto' => 'Projetos'],
+        ['href' => 'https://github.com/kerlonlh', 'target'=> '_blank', 'texto' => 'Github'],
+        ['href' => 'https://www.linkedin.com/in/kerlon-leonardi-hinterholz-958872285/', 'target'=> '_blank', 'texto' => 'Linkedin'],
+    ];
+
+?>
+
 <header class="mx-auto max-w-screen-lg px-3 py-6 flex justify-between">
-        <div class="font-bold text-xl text-cyan-600">
-            🐧 Meu Portfolio ...
-        </div>
-        <div>
-            <ul class="flex gap-x-3 font-medium text-gray-200">
-                <li><a href="#projetos" class="hover:underline">Projetos</a></li>
-                <li><a href="https://github.com/kerlonlh" target="_blank" class="hover:underline">Github</a></li>
-                <li><a href="https://www.linkedin.com/in/kerlon-leonardi-hinterholz-958872285/" target="_blank" class="hover:underline">Linkedin</a></li>
-            </ul>
-        </div>
-    </header>
+    <div class="font-bold text-xl text-cyan-600">
+        🐧 Meu Portfolio ...
+    </div>
+    <div>
+        <ul class="flex gap-x-3 font-medium text-gray-200">
+            <?php foreach ($itens as $item): ?>
+                <li>
+                    <a href="<?=$item['href']?>" class="hover:underline" target="<?=$item['target']?>">
+                        <?=$item['texto']?>
+                    </a>
+                </li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -1,30 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="shortcut icon" href="./assets/avatar.svg" type="image/x-icon">
-</head>
-<body class="bg-slate-900 text-white">
-    <?php include('./componentes/header.php'); ?>
-
-    <main class="mx-auto max-w-screen-lg min-h-20 px-3 py-3">
-        <!--  Hero-->
-        <?php include('./componentes/hero.php'); ?>
-        <!-- Projetos -->
-         <section class="space-y-3 py-6">
-            <h2 class="text-2xl font-bold" id="projetos">Meus projetos</h2>
-            <!-- projeto -->
-            <?php include('./componentes/projetos.php'); ?>
-            
-         </section>
-    </main>
-    <footer class="mx-auto max-w-screen-lg min-h-20">
-        <div class="border-t border-gray-600 pt-6 px-3 text-gray-400 text-sm">
-            @ Copyright <?=date('Y') ?>. Kerlon Hinterholz.
-        </div>
-    </footer>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Portfolio</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <link rel="shortcut icon" href="./assets/avatar.svg" type="image/x-icon">
+    </head>
+    <body class="bg-slate-900 text-white">
+        <!-- Header -->
+        <?php include('./componentes/header.php'); ?>
+        <main class="mx-auto max-w-screen-lg min-h-20 px-3 py-3">
+            <!--  Hero-->
+            <?php include('./componentes/hero.php'); ?>
+            <!-- Projetos -->
+            <section class="space-y-3 py-6">
+                <h2 class="text-2xl font-bold" id="projetos">Meus projetos</h2>
+                <!-- projeto -->
+                <?php include('./componentes/projetos.php'); ?>
+            </section>
+        </main>
+        <footer class="mx-auto max-w-screen-lg min-h-20">
+            <div class="border-t border-gray-600 pt-6 px-3 text-gray-400 text-sm">
+                @ Copyright <?=date('Y') ?>. Kerlon Hinterholz.
+            </div>
+        </footer>
+    </body>
 </html> 
