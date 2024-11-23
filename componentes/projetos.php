2a69e21b58cd8c28e8c74bf7ad033d4d84fb7a25
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2024-11-23 17:24:48 -0300

    Aprimorando os projetos
    Adicionado as imagens, cores das linguagens utilizadas e ajustado as descrições

--- componentes/projetos.php
+++ componentes/projetos.php
@@ -4,32 +4,40 @@ $projetos =[
         "titulo" => "Tech Bin",
         "finalizado" => true,
         "ano" => 2023,
-        "descricao" => "Projeto de uma lixeira inteligente, capaz de detectar automaticamente quando estiver cheia e exibir um contador do seu status",
+        "descricao" => "A Tech Bin é uma lixeira inteligente, que tem por objetivo proporcionar um local apropriado para descartes dos lixos eletrônicos. Por meio de sensores, ela monitora em tempo real o estado em que se encontra sua capacidade.",
         "stack" => ["C++", "HTML", "CSS", "JavaScript"],
-        "img" => ""
+        "img" => "./assets/logo-techbin.png",
+        "alt" => "Logo Tech Bin "
     ],
     [
-        "titulo" => "SGI",
+        "titulo" => "Sistema Gerenciador de Inventário",
         "finalizado" => false,
         "ano" => 2024,
-        "descricao" => "Projeto de uma lixeira inteligente, capas de detectar automaticamente quando estiver cheia e exibir um contador do seu status",
+        "descricao" => "Sistema simples para gerência de estoques, inventário, controle e segurança. Em termos práticos, o SGI é um atrativo para comerciantes, empresas de pequeno porte, restaurantes que não possuem ferramentas facilitadoras de manuseio de inventário.",
         "stack" => ["PHP", "HTML", "CSS", "JavaScript"],
-        "img" => ""
+        "img" => "./assets/logo-sgi.png",
+        "alt" => "Logo Sistema Gerenciador de Inventário"
     ],
     [
-        "titulo" => "Portfolio",
-        "finalizado" => false,
-        "ano" => 2024,
-        "descricao" => "Projeto de uma lixeira inteligente, capaz de detectar automaticamente quando estiver cheia e exibir um contador do seu status",
-        "stack" => ["PHP", "HTML", "CSS", "JavaScript"],
-        "img" => ""
+        "titulo" => "FyloProject",
+        "finalizado" => true,
+        "ano" => 2023,
+        "descricao" => "A FyloProject é uma atividade da Frontend Mentor, proposta nas aulas do curso de Desenvolvimento Web Front-end. Após o termino do curso, melhorei a responsividade e pratiquei Flexbox, além de implementar um menu responsivo e moderno.",
+        "stack" => ["HTML", "CSS", "JavaScript"],
+        "img" => "./assets/logo-fylo.png",
+        "alt" => "Logo FyloProject "
     ],
 ];
+$colors = [
+    "PHP" => 'fuchsia',
+    "HTML" => 'lime',
+    "CSS" => 'sky',
+    "JavaScript" => 'rose'];
 ?>
 <?php foreach ($projetos as $projeto): ?>
 <div class="bg-slate-800 rounded-lg p-3 flex items-center space-x-3">
     <div class="w-1/5 flex items-center justify-middle">
-        <img src="<?=$projeto['img']?>" class="h-42 rounded-md" />
+        <img src="<?=$projeto['img']?>" class="h-28 w-48 rounded-md" alt="<?=$projeto['alt']?>" />
     </div>
     <div class="w-4/5 space-y-3">
         <div class="flex gap-3 justify-between">
@@ -43,11 +51,10 @@ $projetos =[
                 <?php endif; ?>
             </h3>
             <div class="space-x-1">
-                <?php 
-                    $colors = ['fuchsia','lime','sky','rose'];
-                    foreach($projeto['stack'] as $posicao => $language): 
+                <?php foreach($projeto['stack'] as $language):
+                        $color = $colors[$language] ?? 'indigo'; 
                 ?>
-                <span class="bg-<?=$colors[$posicao]?>-400 text-<?=$colors[$posicao]?>-900 rounded-md px-2 py-1 font-semibold text-xs ">
+                <span class="bg-<?=$color?>-400 text-<?=$color?>-900 rounded-md px-2 py-1 font-semibold text-xs ">
                     <?=$language?>
                 </span>
                 <?php endforeach; ?>
